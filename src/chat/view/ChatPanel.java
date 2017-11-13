785d760020857968c3e6136f2e7328656a889efa
X1Warrior <zsto7638@gmail.com>
2017-11-13 13:45:27 -0700

    Created Chat Frame and ChatPanel and made the framework for them.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,15 @@
 package chat.view;
 
-public class ChatPanel
+import javax.swing.JPanel;
+import chat.controller.ChatbotController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatPanel extends JPanel
 {
 
 }
