7d1dbd2e0bb9755a08a27a62434bb91c51c86225
X1Warrior <zsto7638@gmail.com>
2017-11-15 12:44:40 -0700

    setup more imports, more declarations, and my setup sections.

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -2,21 +2,34 @@ package chat.controller;
 
 import chat.model.Chatbot;
 import chat.view.PopupDisplay;
+import chat.view.ChatFrame;
 
 public class ChatbotController
 
 {
-	private PopupDisplay display = new PopupDisplay();
-	private Chatbot chatbot = new Chatbot("Zach");
-	public void start()
+	private Chatbot chatbot;
+	private PopupDisplay display;
+	private ChatFrame appFrame;
+	
+	
+	public ChatbotController()
+	
+	{
+	    display = new PopupDisplay();
+		chatbot = new Chatbot("Zach");
+		appFrame = new ChatFrame(this);
+	}
+		public void start()
+	
+	
 	{
 		String response = display.collectResponse("What do you want to talk about?");
 
-		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
-		{
-			response = popupChat(response);
-			response = display.collectResponse(response);
-		}
+//		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
+//		{
+//			response = popupChat(response);
+//			response = display.collectResponse(response);
+//		}
 	}
 
 	private String popupChat(String chat)
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -3,6 +3,8 @@ package chat.view;
 import javax.swing.JPanel;
 import chat.controller.ChatbotController;
 import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
 import javax.swing.JButton;
 import javax.swing.SpringLayout;
 
@@ -11,5 +13,38 @@ import java.awt.event.ActionListener;
 
 public class ChatPanel extends JPanel
 {
-
+		private ChatbotController appController;
+		private JButton chatButton;
+		private JTextField inputField;
+		private JTextArea chatArea;
+		private SpringLayout appLayout;
+		
+		public ChatPanel(ChatbotController appController)
+		{
+			
+		}
+		
+		private void setupPanel()
+		{
+			
+		}
+		
+		private void setupLayout()
+		{
+			
+		}
+		
+		private void setupListeners()
+		{
+			
+		}
+		
+	
+	
+	
+	
+	
+	
+	
+	
 }
